0de6fa8b277d76545f1fa66969da1a4d58b108a1
danielbenitez <daniel29052003@gmail.com>
2024-07-01 09:32:55 -0600

    responsive+javascript

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 window.addEventListener('scroll' , () =>{
     const HEADER = document.querySelector('.header')
     HEADER.classList.toggle('sticky' , window.scrollY > 5)
+    })
 
+const ABRIR_MENU = document.querySelector('.menu-hamburguesa')
+const MENU_CONTENEDOR = document.querySelector('.navbar')
+const CERRAR_MENU = document.querySelector('.cerrar__menu')
+ABRIR_MENU.addEventListener('click' , () =>{
+    MENU_CONTENEDOR.style.display = 'flex'
 })
+CERRAR_MENU.addEventListener('click' , (e) =>{
+    e.preventDefault()
+    MENU_CONTENEDOR.style.display = 'none'
+})
+
